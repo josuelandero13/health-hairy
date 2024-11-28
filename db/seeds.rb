c6c1e4443e01b2a599d7ce49cc3f54a917e9776c
josuelandero13 <josue.landero130899@gmail.com>
2024-11-27 18:06:48 -0600

    - styles with tailwind - changes in views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,113 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# frozen_string_literal: true
+
+User.destroy_all
+AppointmentType.destroy_all
+City.destroy_all
+Local.destroy_all
+
+user1 = User.create!(
+  first_name: 'tester1',
+  last_name: 'tester petspa',
+  username: 'tester1',
+  email: 'tester1@petspa.com',
+  phone: '+56 999 999 999',
+  password: '12345678',
+  role: 2
+)
+
+# user2 = User.create!(
+#   first_name: 'tester2',
+#   last_name: 'tester petspa',
+#   username: 'tester2',
+#   email: 'tester2@petspa.com',
+#   phone: '+56 999 999 998',
+#   password: '12345',
+#   role: 0
+# )
+
+# pet1 = Pet.create!(
+#   name: 'Firulais', breed: 'Poodle',
+#   important_data: 'Muerde a veces', user: user2,
+#   kind: 0
+# )
+
+city1 = City.create!(name: 'Leon')
+city2 = City.create!(name: 'San Marcos')
+city3 = City.create!(name: 'New York')
+city4 = City.create!(name: 'Buenos Aires')
+city5 = City.create!(name: 'Santiago')
+
+local1 = Local.create!(
+  address: 'Av. Los Pinos 123',
+  city: city1
+)
+local2 = Local.create!(
+  address: 'Av. Los Arandanos 123',
+  city: city2
+)
+local3 = Local.create!(
+  address: 'Av. Pizarro 123',
+  city: city3
+)
+
+appointment_type1 = AppointmentType.create!(
+  name: 'Baño', payment_required: true,
+  user: user1, price: 100
+)
+
+appointment_type2 = AppointmentType.create!(
+  name: 'Baño y corte', payment_required: true,
+  user: user1, price: 180
+)
+
+appointment_type3 = AppointmentType.create!(
+  name: 'Session spa premium', payment_required: true,
+  user: user1, price: 250
+)
+
+puts 'Seed finished!'
+
+# 15.times do |t|
+#   appointment = Appointment.create!(
+#     start_at: DateTime.now + t.day,
+#     end_at: DateTime.now + t.day + 1.hour,
+#     pet: pet1, client: user2,
+#     appointment_type: appointment_type1,
+#     status: 0, local: local1
+#   )
+#   payment = Payment.create!(
+#     appointment: appointment,
+#     user: user2
+#   )
+#   payment.receipt.attach(io: URI.open(Faker::LoremFlickr.image), filename: 'receipt_image.jpg')
+# end
 #
-# Example:
+# 20.times do |t|
+#   appointment = Appointment.create!(
+#     start_at: DateTime.now + t.day,
+#     end_at: DateTime.now + t.day + 1.hour,
+#     pet: pet1, client: user2,
+#     appointment_type: appointment_type1,
+#     status: 0, local: local2
+#   )
+#   payment = Payment.create!(
+#     appointment: appointment,
+#     user: user2
+#   )
+#   payment.receipt.attach(io: URI.open(Faker::LoremFlickr.image), filename: 'receipt_image.jpg')
+# end
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 25.times do |t|
+#   appointment = Appointment.create!(
+#     start_at: DateTime.now + t.day,
+#     end_at: DateTime.now + t.day + 1.hour,
+#     pet: pet1, client: user2,
+#     appointment_type: appointment_type1,
+#     status: 0, local: local3
+#   )
+#   payment = Payment.create!(
+#     appointment: appointment,
+#     user: user2
+#   )
+#   payment.receipt.attach(io: URI.open(Faker::LoremFlickr.image), filename: 'receipt_image.jpg')
+# end
